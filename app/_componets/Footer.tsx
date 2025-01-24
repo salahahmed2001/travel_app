85d929404ac72bef1ddcdf192efbfcd6ac205472
salahahmed2001 <salah01068441@gmail.com>
2025-01-24 18:57:56 +0200

    add travel-app

---
+++ app/_componets/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' text-red-500 w-44 h-11 bg-gray-900'>
+      footer
+    </div>
+  )
+}
+
+export default Footer
